6e08727a4b2dea43e8b6171d6fd4946153a57304
David Adrian <davidcadrian@gmail.com>
2014-02-21 14:53:27 -0500

    Reorganize CMakeLists to build tests
    Links against a static library

---
+++ test/test_shard.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "iterator.h"
+
+int main() {
+	printf("%s\n", "Hello world!");
+	iterator_t *it = iterator_init(1, 0, 1);
+	printf("%u\n", iterator_get_sent(it));
+	return 0;
+}
